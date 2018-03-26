6684fe0b11ffa83ec0f067cfd7047f791ccd9ad5
Connor Meehan <cmeehan@elon.edu>
2018-03-26 16:25:56 -0400

    Initial Commit

---
+++ Utterfare Vendor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Utterfare Vendor
+//
+//  Created by Connor Meehan on 3/26/18.
+//  Copyright © 2018 CBM Web Development. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
